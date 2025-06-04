a03fd1354864a7adf1a58d173594135d22cd1992
langadev <alfredolanga360@gmail.com>
2025-06-04 14:28:25 +0200

    first commit

---
+++ src/main/java/com/vestuario/alugel/AlugelApplication.java
@@ -0,0 +1,13 @@
+package com.vestuario.alugel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vestuario/alugel/AlugelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vestuario.alugel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
